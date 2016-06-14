035c162c928d819f1bd59a98b69ff7d2380d9f4d
goutham <gm7y8@mail.umkc.edu>
2016-06-14 06:32:45 -0500

    fixed logic to calculate byte ranges exactly

--- python/paralle.py
+++ python/paralle.py
@@ -10,15 +10,21 @@ def millis():
 
 def byte_range_divide(l,n):
   len_seg=l/n
+  #print len_seg
   rem=l%n
+  #print rem
   lst=[]
   for i in range(0,n):
-    min=len_seg*(i)
-    max=len_sub*(i+1)
-    if i=n-1:
-      max+=diff
+    min=len_seg*i
+    max=len_seg*(i+1)
+    if i==n-1:
+      max+=rem
     lst.append([min,max])
+  #print lst
   return lst
+
+#print byte_range_divide(27945615,5)
+
 def http_get(url):
   start_time = millis() 
   temp_url=url.split('|')
@@ -31,16 +37,15 @@ def http_get(url):
 
 def create_urls(url):
   r=requests.head(url)
-  temp=byte_range_divide(int(r.headers['content-length']),10)  
-  t=[len(x) for x in temp]
-  y = [sum(t[:i+1]) for i in range(len(t))]
-  y.append(0)
-  y.sort()
+  length=int(r.headers['content-length'])
+  #print length
+  y=byte_range_divide(length,10)  
   lst=[];
   for i in range(0,len(y)-1):	
-     	t=[url,y[i],y[i+1]]
+     	t=[url,y[i][0],y[i][1]]
         l='|'.join(str(e) for e in t)
-        lst.append(l) 	
+        lst.append(l)
+  print lst 	
   return lst
 
 url="http://www-itec.uni-klu.ac.at/ftp/datasets/DASHDataset2014/BigBuckBunny/15sec/bunny_3748236bps/BigBuckBunny_15snonSeg.mp4"

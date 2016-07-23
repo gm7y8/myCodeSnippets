6e67af09dbc4ae8a38f533ca1c2c3c2b5d1fa4d5
goutham <gm7y8@mail.umkc.edu>
2016-07-23 17:02:30 -0500

    Update sdnflows.py

--- sdnflows.py
+++ sdnflows.py
@@ -6,8 +6,54 @@ flow1={
     "dpid": 1,
     "priority": 22222,
     "match":{
+        "in_port":2
+         "dl_dst":"00:00:00:00:00:01"
+    },
+    "actions":[
+        {
+            "output": 1
+        }
+    ]
+ }
+ 
+ 
+flow2={
+    "dpid": 1,
+    "priority": 22222,
+    "match":{
+        "in_port":1
+         "dl_dst":"00:00:00:00:00:02"
+    },
+    "actions":[
+        {
+            "output": 2
+        }
+    ]
+ }
+ 
+ 
+
+flow3={
+    "dpid": 1,
+    "priority": 22222,
+    "match":{
         "in_port":1
          "dl_dst":"00:00:00:00:00:01"
+    },
+    "actions":[
+        {
+            "output": 2
+        }
+    ]
+ }
+ 
+ 
+flow4={
+    "dpid": 1,
+    "priority": 22222,
+    "match":{
+        "in_port":2
+         "dl_dst":"00:00:00:00:00:02"
     },
     "actions":[
         {

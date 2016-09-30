55af7561f10c934d51b9382de7633d1ffd8e6894
goutham <gm7y8@mail.umkc.edu>
2016-09-30 18:45:38 -0500

    working version

--- python/Ibm.py
+++ python/Ibm.py
@@ -1,12 +1,3 @@
-def find_relationship(g,node1,node2):
-	if g.has_key(node1) and g.has_key(node2):
-		if g.get(node1) == g.get(node2):
-			print "found manager"
-			print g.get(node1)
-		else:
-			find_relationship(g,g.get(node1),g.get(node2))
-
-
 def find_manager(g,node,path):
 	path=path+[node]
 	if g.has_key(node):
@@ -14,35 +5,26 @@ def find_manager(g,node,path):
 		if new_path: return new_path
 	return path
 
-
-
 var=raw_input()
 l=var.split(",")
 length=len(l)
 node1=l[length-2]
 node2=l[length-1]
-print node1,node2
+#print node1,node2
 g={}
 for el in l[0:length-2]:
-	print el
+	#print el
 	kv=el.split("->")
-	print kv[0],kv[1]
-	#if g.has_key(kv[1]):
-	#	g[kv[1]].append(str(kv[0]))
-	#else:
-		#g[kv[1]]=[kv[0]]
+	#print kv[0],kv[1]
 	g[kv[1]]=kv[0]
-print str(g)
-#print find_relationship(g,node1,node2)
+#print str(g)
 
-#print g.get(node1) == g.get(node2)
+x=find_manager(g,'D',[]);
 
-print find_manager(g,'E',[])
+y=find_manager(g,'B',[]);
 
-print find_manager(g,'F',[])
-
-man=list(set(find_manager(g,'E',[])) & set(find_manager(g,'F',[])));
-
-print man
-
-
+#print x,y
+for i in x:
+	if i in y:
+		print i
+		break
